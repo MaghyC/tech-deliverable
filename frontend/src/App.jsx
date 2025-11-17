7fedcfcbcc82e25dceca5149df2522c0b5ef6c70
MaghyC <129014665+MaghyC@users.noreply.github.com>
2025-11-16 18:15:41 -0800

    Submitting Quotes
    Adds function handleInputChange and function  handleFormSubmit to prevent the pages reload after submiting new quotes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import "./App.css";
 
 function QuoteItem({ name, message, time }) {
@@ -10,6 +9,12 @@ function QuoteItem({ name, message, time }) {
         </div>
     );
 }
+
+
+   
+
+
+
 
 
 function App() {
@@ -23,6 +28,37 @@ function App() {
             .then(setQuotes);
     }, [filter]);
 
+	// Define state hooks 
+ 	const [form, setForm] = useState({ name: "", message: "" });
+
+	// Handler uses setForm from state above
+	function handleInputChange(e) {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	}
+
+	async function handleFormSubmit(e) {
+		// prevent default reload pages for submit
+		e.preventDefault();
+		// FormData require by the post function
+		const formData = new FormData();
+		formData.append("name", form.name);
+		formData.append("message", form.message);
+
+		const res = await fetch("/api/quote", {
+			method: "post",
+			body: formData
+			
+		});
+		// If your backend keeps returning a redirect, you may not get JSON here,
+		// so just clear the form or optionally reload data from the backend.
+		setForm({ name: "", message: "" });
+		// re-fetch quotes
+		fetch(`/api/quotes?max_age=${filter}`)
+			.then(res => res.json())
+			.then(setQuotes);
+	}
+
+
 
 	return (
 		<div className="App">
@@ -30,18 +66,20 @@ function App() {
 			<h1>Hack at UCI Tech Deliverable</h1>
 
 			<h2>Submit a quote</h2>
-			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form action="/api/quote" method="post">
+			{/*  √√√  TODO: implement custom form submission logic to not refresh the page */}
+			
+			<form onSubmit={handleFormSubmit}>
 				<label htmlFor="input-name">Name</label>
-				<input type="text" name="name" id="input-name" required />
+				<input type="text" name="name" id="input-name" required value={form.name} onChange={handleInputChange}/>
 				<label htmlFor="input-message">Quote</label>
-				<input type="text" name="message" id="input-message" required />
+				<input type="text" name="message" id="input-message" required value={form.message} onChange={handleInputChange}/>
 				<button type="submit">Submit</button>
 			</form>
 
 			<h2>Previous Quotes</h2>
-			{/* TODO: Display the actual quotes from the database */}
+			{/* √√√ TODO: Display the actual quotes from the database */}
 			 <label htmlFor="quote-filter">View quotes from: </label>
+			 {/* select menu */}
             <select
                 id="quote-filter"
                 value={filter}
@@ -56,6 +94,7 @@ function App() {
                 {quotes.map((q, i) => (
                     <QuoteItem {...q} key={i} />
                 ))}
+			
             </div>
         </div>
     );
